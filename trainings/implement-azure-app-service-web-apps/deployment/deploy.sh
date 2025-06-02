e8d87446fbdc3b9b918e7a496259493effa90512
NicoGrassetto <nicograssetto@gmail.com>
2025-06-02 16:40:55 +0200

    Refactor deployment script to simplify Azure App Service deployment process and add configuration file for defaults

--- trainings/implement-azure-app-service-web-apps/deployment/deploy.sh
+++ trainings/implement-azure-app-service-web-apps/deployment/deploy.sh
@@ -1,40 +1,23 @@
 #!/bin/bash
-# Deploy Azure App Service infrastructure and upload static site content
+# Deploy Azure App Service with static HTML content using az webapp up
 
 set -e
 
 RESOURCE_GROUP="counter-rg"
 LOCATION="westeurope"
-APP_SERVICE_PLAN="webAppServicePlan"
 WEBAPP_NAME="counterWebApp"  # Use a fixed name to avoid duplicates
 
-# Delete existing app if it exists
-echo "Checking for existing app: $WEBAPP_NAME in resource group: $RESOURCE_GROUP..."
-if az webapp show --name $WEBAPP_NAME --resource-group $RESOURCE_GROUP &> /dev/null; then
-  echo "App exists. Deleting..."
-  az webapp delete --name $WEBAPP_NAME --resource-group $RESOURCE_GROUP
-  echo "Deleted existing app: $WEBAPP_NAME"
-else
-  echo "No existing app found."
-fi
-
-# Deploy Bicep infrastructure
-az group create --name $RESOURCE_GROUP --location $LOCATION
-az deployment group create \
-  --resource-group $RESOURCE_GROUP \
-  --template-file trainings/implement-azure-app-service-web-apps/deployment/infra/main.bicep \
-  --parameters appServicePlanName=$APP_SERVICE_PLAN webAppName=$WEBAPP_NAME location=$LOCATION
-
-# Zip and deploy static site content
+# Change to the code directory containing the HTML files
 cd trainings/implement-azure-app-service-web-apps/deployment/code
-zip -r site.zip .
-cd ../../../..
 
-az webapp deploy \
+# Deploy using az webapp up with --html flag
+# This command will create the resource group, app service plan, and web app if they don't exist
+# and deploy the HTML content in one step
+az webapp up \
   --resource-group $RESOURCE_GROUP \
+  --location $LOCATION \
   --name $WEBAPP_NAME \
-  --src-path trainings/implement-azure-app-service-web-apps/deployment/code/site.zip \
-  --type zip
+  --html
 
 echo "Deployment complete!"
 echo "App URL: https://$WEBAPP_NAME.azurewebsites.net"
